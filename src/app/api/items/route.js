9ee9214b97cb1fe9091c2dd5a417aba466ca6f4a
sumai-suchi <sumaiyamoina@gmail.com>
2026-07-04 12:33:43 +0600

    adde reduce method to calculate the sum of all items

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,10 +1,14 @@
 export async function POST(request) {
-    console.log("Request received");
+    // console.log("Request received");
   const items = await request.json();
 
   console.log("Items: ", items);
 
+  const  total = items.reduce((sum ,item)=> sum + item.price ,0)
+  console.log (total)
+
   return Response.json({
     message: "Data received successfully!",
+    total: total
   });
 }
--- src/components/ItemForms.jsx
+++ src/components/ItemForms.jsx
@@ -33,8 +33,9 @@ export default function ItemForm() {
     },
     body: JSON.stringify(items),
   });
-
-  console.log(response);
+    
+  const data = await response.json()
+  console.log(data);
 
 
   };
